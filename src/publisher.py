f4e28610f9407d61653d997b155213128765f858
uzmah04 <zu.goomany@gmail.com>
2021-02-25 14:06:26 +0400

    Forth commit

--- src/publisher.py
+++ src/publisher.py
@@ -1,54 +1,53 @@
 #!/usr/bin/env python
 
-import math
 import rospy
-from std_msgs.msg import String
+from std_msgs.msg import Bool
+from Tkinter import *
 from geometry_msgs.msg import PoseStamped
 
-def talker():
-    pub = rospy.Publisher('chatter', PoseStamped, queue_size=10)
-    rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(15) # 15hz
+def callback(data):
+    if (data.data == True):
+	pub = rospy.Publisher('chatter', PoseStamped, queue_size=10)
+	rate = rospy.Rate(15) # 15hz
 
-    Coordonnee = PoseStamped()
-    print(Coordonnee)
+	Coordonnee = PoseStamped()
 
-    Coordonnee.pose.position.x = 0
-    Coordonnee.pose.position.y = 0
+	Coordonnee.pose.position.x = 0
+	Coordonnee.pose.position.y = 0
 
-    a = 0
-    b = 0
-    while not rospy.is_shutdown():
+	a = 0
+	b = 0
 
 	if (Coordonnee.pose.position.x == 0 & Coordonnee.pose.position.y == 0):
-		while (Coordonnee.pose.position.y != 8):
-			Coordonnee.pose.position.y = a
-			a = a + 1
-			pub.publish(Coordonnee)
-			rate.sleep()
+	    while (Coordonnee.pose.position.y != 8):
+		Coordonnee.pose.position.y = a
+		a = a + 1
+		pub.publish(Coordonnee)
+		rate.sleep()
 
-		while (Coordonnee.pose.position.x != 8):
-			Coordonnee.pose.position.x = b
-			b = b + 1
-			pub.publish(Coordonnee)
-			rate.sleep()
+	    while (Coordonnee.pose.position.x != 8):
+		Coordonnee.pose.position.x = b
+		b = b + 1
+		pub.publish(Coordonnee)
+		rate.sleep()
 
 	elif (Coordonnee.pose.position.x == 8 & Coordonnee.pose.position.y == 8):
-		while (Coordonnee.pose.position.y != 0):
-			a = a - 1
-			Coordonnee.pose.position.y = a
-			pub.publish(Coordonnee)
-			rate.sleep()
+	    while (Coordonnee.pose.position.y != 0):
+		a = a - 1
+		Coordonnee.pose.position.y = a
+		pub.publish(Coordonnee)
+		rate.sleep()
 
-		while (Coordonnee.pose.position.x != 0):
-			b = b - 1
-			Coordonnee.pose.position.x = b
-			pub.publish(Coordonnee)
-			rate.sleep()
+	    while (Coordonnee.pose.position.x != 0):
+		b = b - 1
+		Coordonnee.pose.position.x = b
+		pub.publish(Coordonnee)
+		rate.sleep()
 
+def listen():
+    rospy.init_node('listen', anonymous=True)
+    rospy.Subscriber('button_state', Bool, callback)
+    rospy.spin()
 
 if __name__ == '__main__':
-    try:
-        talker()
-    except rospy.ROSInterruptException:
-        pass
+    listen()
